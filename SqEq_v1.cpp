882d67713b52febeea0bb3fc99d32bf8782373fb
SieC65 <Shalexey91@mail.ru>
2016-07-18 18:57:14 +0300

    Update SqEq_v1.cpp
    Added checking for a == 0

--- SqEq_v1.cpp
+++ SqEq_v1.cpp
@@ -5,6 +5,7 @@ int SqEq_Ev (double, double, double, double*, double*);
 int SqEq_Pr (double, double, double, double*, double*, int);
 int SqEq_Test (double*, double*);
 int SqEq_Sc (double*, double*, double*);
+const int INF = -1;
 
 int main ()
 {
@@ -30,10 +31,10 @@ int main ()
 
 int SqEq_Test (double* x1, double* x2)
 {
-    const int Np = 4;
-    double a[Np] = {1, 1, -1, -2};
-    double b[Np] = {-2, -3, 3, 6};
-    double c[Np] = {1, 4, 0, -5};
+    const int Np = 7;
+    double a[Np] = { 1,  0,   1,   0,  0,  -1,  0};
+    double b[Np] = {-3,  0,  -2,   1,  1,   3,  0};
+    double c[Np] = { 4,  1,   1,  -5,  0,   0,  0};
     printf ("\n--------TEST--------\n");
     for (int i = 0; i < Np; i++)
     {
@@ -59,22 +60,39 @@ int SqEq_Ev (double a, double b, double c, double* x1, double* x2)
 {
     assert (x1 != x2);
     double d = 0;
-    d = (b*b - 4*a*c);
-    if (d < 0)
+    if (a == 0)
     {
-        return (0);
-    }
-    else if (d == 0)
-    {
-        *x1 = (-b + sqrt(d)) / 2;
-        *x2 = *x1;
-        return (1);
+        if (b == 0)
+        {
+            if (c == 0) return INF;
+            else return (0);
+        }
+        else
+        {
+            *x1 = (-c/b);
+            *x2 = *x1;
+            return (1);
+        }
     }
     else
     {
-        *x1 = (-b + sqrt(d)) / 2;
-        *x2 = (-b - sqrt(d)) / 2;
-        return (2);
+        d = (b*b - 4*a*c);
+        if (d < 0)
+        {
+            return (0);
+        }
+        else if (d == 0)
+        {
+            *x1 = (-b + sqrt(d)) / 2;
+            *x2 = *x1;
+            return (1);
+        }
+        else
+        {
+            *x1 = (-b + sqrt(d)) / 2;
+            *x2 = (-b - sqrt(d)) / 2;
+            return (2);
+        }
     }
 }
 
@@ -82,13 +100,8 @@ int SqEq_Pr (double a, double b, double c, double* x1, double* x2, int N_Roots)
 {
     assert (x1 != x2);
     printf ("\nFor a = %.1f, b = %.1f, c = %.1f ", a, b, c);
-    if (N_Roots == 0)
-    {
-        printf ("\n there are no roots \n");
-    }
-    else if (N_Roots == 1)
-    {
-        printf ("\n there is one root: \n  x1 = %.1f \n", *x1);
-    }
-    else printf ("\n there are two roots: \n  x1 = %.1f \n  x2 = %.1f \n", *x1, *x2);
+    if (N_Roots == 0) printf ("\n there are no roots \n");
+    else if (N_Roots == 1) printf ("\n there is one root: \n  x1 = %.1f \n", *x1);
+    else if (N_Roots == 2) printf ("\n there are two roots: \n  x1 = %.1f \n  x2 = %.1f \n", *x1, *x2);
+    else if (N_Roots == -1) printf ("\n there are infinitive amount of roots \n");
 }
